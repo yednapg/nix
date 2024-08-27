c7fca9d239be38eb3a27e3c9406b7a065b72e515
Gaurav Pandey <hi@pandeygaurav.com>
2024-08-27 14:01:06 +0530

    update

--- flake.nix
+++ flake.nix
@@ -25,32 +25,49 @@
         };
         security.pam.enableSudoTouchIdAuth = true;
         programs.zsh.enable = true;
-        # nix-darwin packages
-        environment.systemPackages = [ ];
+        
+	# nix-darwin packages
+        environment.systemPackages = [ 
+                    
+        ];
         homebrew = {
           enable = true;
           onActivation.cleanup = "uninstall";
           taps = [ ];
-          brews = [ ];
-          casks = [ "iterm2" "figma" ];
+          brews = [ "python3" "node" ];
+          casks = [ "figma" "alacritty" "slack" "discord" "zoom" "rectangle" "visual-studio-code" "google-chrome" "spotify" "firefox@developer-edition" ];
         };
-        # macOS settings
+   	
+	# macOS settings
         system.defaults.dock.autohide = true;
       };
+
       homeconfig = { pkgs, ... }: {
         home.stateVersion = "23.05";
         programs.home-manager.enable = true;
-        # home-manager packages
-        home.packages = with pkgs; [ ];
+        
+	# home-manager packages
+        home.packages = with pkgs; [
+	 ];
+
         home.sessionVariables = {
           EDITOR = "vim";
         };
+
         programs.zsh = {
           enable = true;
           shellAliases = {
             switch = "darwin-rebuild switch --flake ~/.config/nix";
           };
         };
+        
+        programs.zsh.dotDir = ".config/zsh";  
+        programs.zsh.initExtra = ''
+          # Powerlevel10k Zsh theme  
+          source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme  
+          test -f ~/.config/zsh/.p10k.zsh && source ~/.config/zsh/.p10k.zsh  
+        '';
+
         programs.neovim = {
           enable = true;
           defaultEditor = false;
@@ -58,14 +75,24 @@
           vimAlias = false;
           vimdiffAlias = false;
           plugins = with pkgs.vimPlugins; [
-            gruvbox-material
           ];
           extraLuaConfig = ''
             vim.o.termguicolors = true
-            vim.cmd('colorscheme gruvbox-material')
-            vim.g.gruvbox_material_background = 'hard'
           '';
         };
+
+        programs.alacritty = {
+          enable = true;
+          settings = {
+            font.size = 18;
+            window = {
+              padding.x = 10;
+              padding.y = 10;
+              decorations = "buttonless";
+            };
+          };
+        };
+
         programs.git = {
           enable = true;
           userName = "Gaurav Pandey";
@@ -74,8 +101,8 @@
           extraConfig = {
             init.defaultBranch = "main";
             push.autoSetupRemote = true;
-          };
-        };
+         };
+	};
       };
     in
     {
